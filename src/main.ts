639f8870366f1e271e856467edd06c12526e8f0c
oneofthezombies <hunhoekim@gmail.com>
2025-03-21 05:02:04 +0000

    chore: Add sub packages

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-console.log("Hello!");
+import { spawn } from "node:child_process";
+import readline from "node:readline";
+
+const child = spawn(`echo hello`, { shell: true, stdio: "pipe" });
+child.on("close", (code) => {
+  console.log(code);
+});
+
+const stdout = readline.createInterface({ input: child.stdout });
+stdout.on("line", (line) => {
+  console.log(line);
+});
+
+const stderr = readline.createInterface({ input: child.stderr });
+stderr.on("line", (line) => {
+  console.error(line);
+});
